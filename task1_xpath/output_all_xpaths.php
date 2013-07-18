cde8f692cf8e3a469afc1d44a46ce21da6d4c9d2
Sam Wyman <sam.wyman@gmail.com>
2013-07-18 15:31:12 -0700

    GeneratorTest

--- task1_xpath/output_all_xpaths.php
+++ task1_xpath/output_all_xpaths.php
@@ -4,7 +4,6 @@
 * @param $xsl
 * @return string xml
 */
-
 function transform($xml, $xsl) {
    $xslt = new XSLTProcessor();
    $xslt->importStylesheet(new SimpleXMLElement($xsl));
@@ -15,4 +14,3 @@ $xsl_worksheet = file_get_contents('all_xpaths.xsl');
 $xml_input = file_get_contents('../sample_data/photos_rss.xml');
 
 print_r( transform($xml_input, $xsl_worksheet) );
-?>
