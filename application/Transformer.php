ccb331b25315e932213dcc95d4706261bf1c6074
Ilya Bakshee <ilya.bakshee@blackboard.com>
2013-07-18 15:31:55 -0700

    working tests for transform

--- application/Transformer.php
+++ application/Transformer.php
@@ -7,6 +7,8 @@ class Transformer
         $xml_body = file_get_contents($filename);
         $xsl_worksheet = file_get_contents(ALL_XSLT);
         $all_xpaths = $this->transform($xml_body, $xsl_worksheet);
+
+        $record_path = NULL;
         foreach (explode("\n", $all_xpaths) as $p1) {
             if (($pos = strpos($p1, "[")) === false) {
 
@@ -15,7 +17,7 @@ class Transformer
                 $record_path = $path_parts[0];
             }
         }
-
+        return $record_path;
     }
 
     function transform($xml, $xsl)
