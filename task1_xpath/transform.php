a9d74bedad8d149445a22cae1bcfca8bb51bbab2
Ilya Bakshee <ilya.bakshee@blackboard.com>
2013-07-18 14:58:31 -0700

    working xpath based

--- task1_xpath/transform.php
+++ task1_xpath/transform.php
@@ -12,7 +12,24 @@ function transform($xml, $xsl) {
 }
 
 $xsl_worksheet = file_get_contents('all_xpaths.xsl');
-$xml_input = file_get_contents('input1.xml');
+$xml_input = file_get_contents('input2.xml');
 
-print_r( transform($xml_input, $xsl_worksheet) );
+$all_xpaths =  transform($xml_input, $xsl_worksheet);
+
+$record_path = null;
+foreach (explode("\n", $all_xpaths) as $p1)
+{
+	if (($pos = strpos($p1, "[")) === false ) {
+
+	} else {
+		$path_parts = explode("[",$p1);
+		$record_path = $path_parts[0];
+	}
+}
+
+if ( isset($record_path) ) {
+	print "Record path: $record_path\n";
+} else {
+	print "Record path NOT FOUND!\n";
+}
 ?>
