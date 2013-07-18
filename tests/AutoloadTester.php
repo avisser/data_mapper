f1a304c4e0fc9aeff56c95d80e79b24a5ba00083
Andy Visser <visser@gmail.com>
2013-07-18 15:01:50 -0700

    re-org and autoload test

--- tests/AutoloadTester.php
+++ tests/AutoloadTester.php
@@ -7,6 +7,12 @@ class AutoloadTester extends TestBase
     function testFoo()
     {
         $foo = new Foo();
-        $this->assertTrue(true); //got here!
+        $this->assertNotNull($foo); //got here!
+    }
+
+    function testNS()
+    {
+        $foo = new FieldMap\Generator(new SimpleXmlElement("<data/>"));
+        $this->assertNotNull($foo); //got here!
     }
 }
